57b6a0f98ec668ef3582f90c1b9f26f3e4d70cf2
Dylan Musil <dmusil@degreed.com>
2020-04-05 14:51:54 -0400

    Create a TeamCity app service using the official Jetbrains TeamCity Docker image. The data is stored in a file share that belongs to an Azure Storage account.

---
+++ main.tf
@@ -0,0 +1,58 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "teamcity_rg" {
+  location = "eastus"
+  name     = "dmteamcity"
+}
+
+resource "azurerm_storage_account" "teamcity_sa" {
+  account_replication_type = "LRS"
+  account_tier             = "Standard"
+  location                 = azurerm_resource_group.teamcity_rg.location
+  name                     = "teamcitydatasa"
+  resource_group_name      = azurerm_resource_group.teamcity_rg.name
+}
+
+resource "azurerm_storage_share" "teamcity_data" {
+  name                 = "teamcitydata"
+  storage_account_name = azurerm_storage_account.teamcity_sa.name
+}
+
+resource "azurerm_app_service_plan" "teamcity_server_asp" {
+  location            = azurerm_resource_group.teamcity_rg.location
+  name                = "teamcityserverasp"
+  resource_group_name = azurerm_resource_group.teamcity_rg.name
+  kind                = "linux"
+  reserved            = true
+  sku {
+    size = "B1"
+    tier = "Basic"
+  }
+}
+
+resource "azurerm_app_service" "teamcity_server_app" {
+  app_service_plan_id = azurerm_app_service_plan.teamcity_server_asp.id
+  location            = azurerm_resource_group.teamcity_rg.location
+  name                = "teamcityserverapp"
+  resource_group_name = azurerm_resource_group.teamcity_rg.name
+  site_config {
+    linux_fx_version = "DOCKER|jetbrains/teamcity-server"
+    always_on = true
+  }
+  app_settings = {
+    "WEBSITES_PORT"="8111"
+    "WEBSITES_ENABLE_APP_SERVICE_STORAGE"=false
+    "DOCKER_REGISTRY_SERVER_URL"="https://index.docker.io"
+    "TEAMCITY_DATA_PATH"="/teamcity-data"
+  }
+  storage_account {
+    access_key = azurerm_storage_account.teamcity_sa.primary_access_key
+    account_name = azurerm_storage_account.teamcity_sa.name
+    name = "teamcity-data"
+    share_name = azurerm_storage_share.teamcity_data.name
+    type = "AzureFiles"
+    mount_path = "/teamcity-data"
+  }
+}
